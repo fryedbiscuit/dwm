2e51ad71e5a4d5a6fca3701145745b976225313c
Bloody-Scythe <vitor.hugo.alves.santos11@gmail.com>
2020-07-13 16:35:52 -0300

    Changed layout order

--- config.def.h
+++ config.def.h
@@ -61,11 +61,11 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+ 	{ "[@]",      spiral },  /* first entry is default */
+ 	{ "[\\]",     dwindle },
+	{ "[]=",      tile },
 	{ "[M]",      monocle },
- 	{ "[@]",      spiral },
- 	{ "[\\]",      dwindle },
+	{ "><>",      NULL },    /* no layout function means floating */
 };
 
 /* key definitions */
@@ -101,11 +101,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
+	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[0]} }, /* Spiral */
+	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[1]} }, /* Dwindle */
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[2]} }, /* Tile */
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[3]} }, /* Monocle */
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[4]} }, /* Floating */
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
