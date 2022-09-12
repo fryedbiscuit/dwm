f16b9b8d5542bef15de3dbf28b58ca0d65d40ba1
Bloody-Scythe <bloodyscythe404@gmail.com>
2022-09-12 00:57:43 -0300

    Changed theme colors to gray

--- config.def.h
+++ config.def.h
@@ -18,10 +18,13 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char selection_color[] = "#555555";
+static const char border_color[]    = "#666666";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/* [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  }, */
+	[SchemeSel]  = { col_gray4, selection_color,  border_color  },
 };
 
 static const char *const autostart[] = {
