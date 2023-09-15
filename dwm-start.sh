40b09a0710cb9404190307352dc27c27039a8912
Bloody-Scythe <bloodyscythe404@gmail.com>
2023-09-15 22:34:02 +0000

    Added autostart programs

--- dwm-start.sh
+++ dwm-start.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 pkill dwm_bar
 dwm_bar &
+
+export GTK_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
+export QT_IM_MODULE=fcitx
+export XIM_PROGRAM=/usr/bin/fcitx
+
+export XMENU="rofi -dmenu"
+export TERMINAL="st"
+
+numlockx on
+
+picom -b
+parcellite &
+nm-applet &
+sxhkd -m 1 &
+
+nitrogen --restore
+
+
 exec dwm
